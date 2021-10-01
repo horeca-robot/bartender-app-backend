62ea9386d87fcb30be96f23e666c9f8645c8bcba
Quint <quint.legius@iqfx.nl>
2021-10-01 16:58:31 +0200

    fixed 401 unauthorized

--- src/main/java/com/horecarobot/backend/BackendApplication.java
+++ src/main/java/com/horecarobot/backend/BackendApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @SpringBootApplication
-@ComponentScan({"edu.fontys.horecarobot.databaselibrary.*"})
-@EnableJpaRepositories("edu.fontys.horecarobot.databaselibrary.*")
+@ComponentScan({"edu.fontys.horecarobot.databaselibrary.*","com.horecarobot.backend.*"})
+@EnableJpaRepositories({"edu.fontys.horecarobot.databaselibrary.*","com.horecarobot.backend.*"})
+@Import(SecurityConfig.class)
 @EntityScan("edu.fontys.horecarobot.databaselibrary.*")
 public class BackendApplication {
 
