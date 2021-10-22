3c6cff0bc064ab529447fc7f24c629bb373d2892
BryanAafjes <432731@student.fontys.nl>
2021-10-22 14:05:07 +0200

    Added functioning H2 database

--- src/test/java/com/horecarobot/backend/ProductTests.java
+++ src/test/java/com/horecarobot/backend/ProductTests.java
@@ -4,14 +4,20 @@ import com.horecarobot.backend.Product.ProductService;
 import edu.fontys.horecarobot.databaselibrary.models.Product;
 import edu.fontys.horecarobot.databaselibrary.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 public class ProductTests {
     private ProductService productService;
     private ProductRepository productRepository;
@@ -39,14 +45,13 @@ public class ProductTests {
     public void Should_Get_All_Products() {
         //Arrange
         Product product1 = new Product(UUID.randomUUID(), "Cola", "imgPath", 1.50, 0, "This is the orignal Coca Cola!", false, null, null);
-        Product product2 = new Product(UUID.randomUUID(), "Cola", "imgPath", 1.50, 0, "This is the orignal Coca Cola!", false, null, null);
+        Product product2 = new Product(UUID.randomUUID(), "Ice Tea", "imgPath", 1.50, 0, "Nice peach ice tea!", false, null, null);
 
         productRepository.save(product1);
         productRepository.save(product2);
 
         //Act
-//        Iterable<Product> products = productService.getAllProducts();
-        Iterable<Product> products = productRepository.findAll();
+        Iterable<Product> products = productService.getAllProducts();
 
         //Assert
         assertThat(products).hasSize(2);
