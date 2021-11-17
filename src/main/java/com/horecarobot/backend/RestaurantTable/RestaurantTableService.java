828e90c9261aee9ee1ce8225e39252fb7389e976
Albion <461340@student.fontys.nl>
2021-11-17 18:27:33 +0100

    Fixed weird text

--- src/main/java/com/horecarobot/backend/RestaurantTable/RestaurantTableService.java
+++ src/main/java/com/horecarobot/backend/RestaurantTable/RestaurantTableService.java
@@ -5,10 +5,7 @@ import edu.fontys.horecarobot.databaselibrary.repositories.RestaurantTableReposi
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
-=======
 
->>>>>>> develop
 import java.util.List;
 import java.util.UUID;
 
