f83da69ed1e741b85cd70a3d958cf47945fc191f
Albion <461340@student.fontys.nl>
2021-12-16 13:56:49 +0100

    cors revert

--- src/main/java/com/horecarobot/backend/BackendApplication.java
+++ src/main/java/com/horecarobot/backend/BackendApplication.java
@@ -40,7 +40,7 @@ public class BackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry)
 			{
-				registry.addMapping("/api/**").allowedOrigins("*");
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:8081", "http://127.0.0.1:8081", "https://rigs.bryanaafjes.nl", "https://rigs.bryanaafjes.nl:444");
 			}
 		};
 	}
